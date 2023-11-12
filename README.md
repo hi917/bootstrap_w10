c49710348d61bcf3eb7d71867930e666ed7de467
Peter <petern91710@gmail.com>
2023-11-11 19:33:11 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Description
+Powershell script to install a suite of applications on a fresh Windows 10 computer using Chocolatey.
+
+# Usage
+```sh
+# Download Powershell script
+wget https://github.com/hi917/bootstrap_w10/choco-install-apps.ps1
+
+# Run script
+powershell -ExecutionPolicy Bypass -File .\choco-install-apps.ps1
+```
---
+++ choco-install-apps.ps1
@@ -0,0 +1,63 @@
+# Description: Install suite of desktop applications
+# Usage: powershell -ExecutionPolicy Bypass -File .\choco-install-apps.ps1
+
+Set-ExecutionPolicy Bypass -Scope Process -Force
+
+# Install Chocolatey, if not installed
+if (!(Test-Path -Path "$env:ProgramData\Chocolatey")) {
+  Invoke-Expression((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+# Define packages to install
+$packages =
+    # Web Browsers
+    'firefox',
+    'googlechrome',
+
+    # Imaging
+    'sharex',
+
+    # Utilities
+    'crystaldiskinfo',
+    'crystaldiskmark',
+    'czkawka',
+    'dupeguru',
+    'git',
+    'hwmonitor',
+    'jdownloader',
+    'wiztree',
+    'yt-dlp',
+
+    # Messaging
+    'discord',
+    'foxitreader',
+    'sublimetext3',
+
+    # Documents
+    'drawio',
+    'openoffice',
+
+    # Online Storage
+    'dropbox',
+
+    # Compression
+    '7zip',
+
+    # Media
+    'vlc',
+
+    # Other
+    'steam',
+    'razer-synapse-3',
+    'ubiquiti-unifi-controller',
+
+    # Security
+    'malwarebytes',
+    'windscribe',
+
+    # Developer Tools
+    'vscode'
+
+ForEach($package in $packages) {
+    choco install $item -fy
+}
